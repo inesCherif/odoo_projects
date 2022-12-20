28296f956c5f237d0c771a03b1c552da4a068dce
unknown <ines.cherif.tn@gmail.com>
2022-12-20 19:49:56 +0100

    first commit

---
+++ Odoo 16.0.20221216/PostgreSQL/pgAdmin 4/web/config_distro.py
@@ -0,0 +1,8 @@
+SERVER_MODE = False
+HELP_PATH = '../../../docs/en_US/html/'
+UPGRADE_CHECK_KEY = 'edb-pgadmin4'
+DEFAULT_BINARY_PATHS = {
+"pg":   "C:\\Program Files\\Odoo 16.0.20221216\\PostgreSQL\\bin",
+"ppas": ""
+}
+                    
